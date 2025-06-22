593164bee04614d6aa61f634369134bf252c9402
Ahmed Hashem <ahmedhashem141296@gmail.com>
2025-06-22 14:48:00 +0300

    Q03Solved

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -7,17 +7,25 @@
             #region Q01
             Console.Write("Enter a number: ");
             string input = Console.ReadLine();
-            int number = Convert.ToInt32(input); 
+            int number = Convert.ToInt32(input);
             Console.WriteLine("You entered: " + number);
 
             #endregion
-
-
             #region Q02
             string str = "123abc";
             int result = Convert.ToInt32(str); // This will throw exception
 
             #endregion
+            #region Q03
+            // برنامج بيجمع رقمين عشريين من نوع float
+            float a = 5.5f;
+            float b = 2.2f;
+            float result = a + b;
+            Console.WriteLine("Result: " + result);
+            #endregion
+          
+
+
         }
     }
 }
