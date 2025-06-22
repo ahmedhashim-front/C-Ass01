17cc6fcb02e735bd926274b8f00b65511546c7c5
Ahmed Hashem <ahmedhashem141296@gmail.com>
2025-06-22 14:57:28 +0300

    Q05 Solved

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -23,12 +23,26 @@
             float result = a + b;
             Console.WriteLine("Result: " + result);
             #endregion
-
-
             #region Q04
             string text = "Hello World";
             string part = text.Substring(6, 5);
             Console.WriteLine(part);
+
+            #endregion
+            #region Q05
+            int a5 = 10;
+            int b5 = a5;
+            b5 = 20;
+
+            Console.WriteLine("a = " + a5);
+            Console.WriteLine("b = " + b5);
+
+             // اللي هيحصل:
+             //هتفضل قيمة a5زي ماهي 10 
+             // لاننا لما عملنا b5 = a5 مغيرناش قيمه a5 
+             // وقيمة المتغير b هتبقى 20 بعد التعديل.
+             // ولما نسخنا a5 إلى b5، كل واحد بقى له نسخة مستقلة من القيمة.
+
 
             #endregion
         }
