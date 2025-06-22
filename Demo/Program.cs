74330a32e568d7c2eb53ceea104d984ec20836d8
Ahmed Hashem <ahmedhashem141296@gmail.com>
2025-06-22 14:26:02 +0300

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+   
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Ahmed Adel");
+            #region MyRegion
+           
+            #endregion
+
+        }
+    }
+}
